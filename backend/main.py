03ee99c0c0747f3f62116b503e1cfe2ed78f72a6
grubsteak <11185290+grubsteak@users.noreply.github.com>
2023-04-07 13:12:28 -0500

    Enable Cross-origin requests for flask, make frontend's ConsumerView access backend API for baseItems

--- backend/main.py
+++ backend/main.py
@@ -7,10 +7,33 @@
 #   cd Project3_Zeta/backend
 #   python3 -m flask --app main run
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+# // const baseItems = [{item: 'Cheese Pizza', id: 1}, 
+# //                     {item:'1 Topping Pizza', id: 2}, 
+# //                     {item:'2-4 Topping Pizza', id: 3},
+# //                     {item:'Drink', id: 4}
+# //                   ];
+@app.route("/baseItems")
+def get_baseitems():
+    baseItems = [
+        {'item_display_name': 'Cheeze Pizza', 'itemtype_id': 1},
+        {'item_display_name': '1 Topping Pizza', 'itemtype_id': 2},
+        {'item_display_name': '2-4 Topping Pizza', 'itemtype_id': 3},
+        {'item_display_name': 'Drink', 'itemtype_id': 4},
+    ]
+    response = jsonify(baseItems)
+
+    response.status_code = 200
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
--- src/ConsumerView.js
+++ src/ConsumerView.js
@@ -1,35 +1,64 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
 
 import './App.css'; // Tell webpack that App.js uses these styles
-import ToppingTabs from './ToppingTabs';
 
 
 
-const baseItems = [{item: 'Cheese Pizza', id: 1}, {item:'1 Topping Pizza', id: 2}, {item:'2-4 Topping Pizza', id: 3}, {item:'Drink', id: 4}];
- 
+// const baseItems = [{item: 'Cheese Pizza', id: 1}, 
+//                     {item:'1 Topping Pizza', id: 2}, 
+//                     {item:'2-4 Topping Pizza', id: 3},
+//                     {item:'Drink', id: 4}
+//                   ];
 
-  
-  export default function ConsumerView(props) {
-    // const [countNum, changeCount] = useState(0);
 
-    // function whenClicked(){
-    //   changeCount(countNum+1);
-    // }
-    const listItems = baseItems.map(baseItems => <div class="card__container"><div class = "card"><button class="cardContent" key={baseItems.id} onClick={() => props.onFormSwitch("toppings_tabs")}> {baseItems.item}</button></div></div>);
-    return (
-      //Parent Element
-      <div>
-        <div class="background">
-          <h2>Choose your Main</h2>
-          {listItems}
-        </div>
-        <footer class="footer">
-          <p>Copyright &copy; 2023 Yezen Hijazin</p>
-        </footer>
+
+export default function ConsumerView(props) {
+  // const [countNum, changeCount] = useState(0);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [baseItems, setItems] = useState([]);
+
+
+  // from https://legacy.reactjs.org/docs/faq-ajax.html
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/baseItems")
+      .then((res) => res.json())
+      .then((res) => {
+        setIsLoaded(true);
+        setItems(res);
+      },
+        (error) => {
+          setIsLoaded(true);
+          console.log("error:", error)
+          alert(error);
+        })
+  }, [])
+
+  // function whenClicked(){
+  //   changeCount(countNum+1);
+  // }
+  return (
+    //Parent Element
+    <div>
+      <div class="background">
+        <h2>Choose your Main</h2>
+        {
+          baseItems.map(item =>
+            <div class="card__container">
+              <div class="card">
+                <button class="cardContent" key={baseItems.id} onClick={() => props.onFormSwitch("toppings_tabs")}>
+                  {item.item_display_name}
+                </button>
+              </div>
+            </div>)
+        }
       </div>
-    );
-  }
+      <footer class="footer">
+        <p>Copyright &copy; 2023 Yezen Hijazin</p>
+      </footer>
+    </div>
+  );
+}
