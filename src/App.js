d0ffa684ab309b94f8d4ba8bf21311cf50aa27ff
Yezen Hijazin <yth77@tamu.edu>
2023-04-07 13:55:37 -0500

    bug fix

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ConsumerView from './ConsumerView';
 import Login from './Login';
 import { SignUp } from './SignUp';
 import CashierView from './Cashier';
+import ToppingTabs from './ToppingTabs';
 
 
 /* Yezen, please uncomment this after you push the Toppings tab */
@@ -25,33 +26,19 @@ const sceneSelector = () => {
 
 
 export default function MyApp() {
-  const [currentForm, setCurrentForm] = useState("login")
-
-  const toggleForm = (formName) => {
+  const [currentForm, setCurrentForm]=useState("login")
+  
+  const toggleForm = (formName) =>{
     setCurrentForm(formName)
   }
   return (
     <div className='App'>
-      {
-        /* Sorry for this compact code if else wasnt working and knew no other way */
-        currentForm === "login" ?
-          <Login onFormSwitch={toggleForm} />
-        : currentForm === "signup" ?
-          <SignUp onFormSwitch={toggleForm} />
-        : currentForm === "consumer_view" ?
-          <ConsumerView onFormSwitch={toggleForm} />
-        : currentForm === "cashier_view" ?
-          <CashierView onFormSwitch={toggleForm} />
-
-        /* Yezen, please uncomment this after you push the Toppings tab */
-         : 
-           currentForm === "toppings_tabs"
-           ? <ToppingTabs onFormSwitch={toggleForm} /> 
-
-        : null // Didn't need an else so I put null and it worked I guess
-        // The names are declared when the buttons are pressed and formswitch is assign with a string. The strings are then matched here.
-        // If the current form is login then stay at login, if its signup then move to sign up screen.
-      }
+    {
+      /*Sorry for this compact code if else wasnt working and knew no other way*/
+      currentForm==="login" ? <Login onFormSwitch={toggleForm}/> : currentForm==="signup" ? <SignUp onFormSwitch={toggleForm}/> : currentForm ==="consumer_view" ?<ConsumerView onFormSwitch={toggleForm}/>: currentForm === "cashier_view" ? <CashierView onFormSwitch={toggleForm}/>: currentForm ==="toppings_tabs"?<ToppingTabs onFormSwitch={toggleForm}/>: null//didnt need an else so i put null and it worked i guess
+      // The names are declared when the buttons are pressed and formswitch is assign with a string. The strings are then matched here.
+      // If the current form is login then stay at login, if its signup then move to sign up screen.
+    }
     </div>
   );
 }
--- src/ConsumerView.js
+++ src/ConsumerView.js
@@ -19,41 +19,46 @@ import './App.css'; // Tell webpack that App.js uses these styles
 export default function ConsumerView(props) {
   // const [countNum, changeCount] = useState(0);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [baseItems, setItems] = useState([]);
+  //const [baseItems, setItems] = useState([]);
+  const baseItems = [{item: 'Cheese Pizza', id: 1}, {item:'1 Topping Pizza', id: 2}, {item:'2-4 Topping Pizza', id: 3}, {item:'Drink', id: 4}];
 
 
   // from https://legacy.reactjs.org/docs/faq-ajax.html
-  useEffect(() => {
-    fetch("http://127.0.0.1:5000/baseItems")
-      .then((res) => res.json())
-      .then((res) => {
-        setIsLoaded(true);
-        setItems(res);
-      },
-        (error) => {
-          setIsLoaded(true);
-          console.log("error:", error)
-          alert(error);
-        })
-  }, [])
+  // useEffect(() => {
+  //   fetch("http://127.0.0.1:5000/baseItems")
+  //     .then((res) => res.json())
+  //     .then((res) => {
+  //       setIsLoaded(true);
+  //       setItems(res);
+  //     },
+  //       (error) => {
+  //         setIsLoaded(true);
+  //         console.log("error:", error)
+  //         alert(error);
+  //       })
+  // }, [])
 
   // function whenClicked(){
   //   changeCount(countNum+1);
   // }
+  const listItems = baseItems.map(baseItems => <div class="card__container"><div class = "card"><button class="cardContent" key={baseItems.id} onClick={() => props.onFormSwitch("toppings_tabs")}> {baseItems.item}</button></div></div>);
+
   return (
     //Parent Element
     <div>
       <div class="background">
         <h2>Choose your Main</h2>
         {
-          baseItems.map(item =>
-            <div class="card__container">
-              <div class="card">
-                <button class="cardContent" key={baseItems.id} onClick={() => props.onFormSwitch("toppings_tabs")}>
-                  {item.item_display_name}
-                </button>
-              </div>
-            </div>)
+          // baseItems.map(item =>
+          //   <div class="card__container">
+          //     <div class="card">
+          //       <button class="cardContent" key={baseItems.id} onClick={() => props.onFormSwitch("toppings_tabs")}>
+          //         {/* {item.item_display_name} */}
+          //         
+          //       </button>
+          //     </div>
+          //   </div>)
+          listItems
         }
       </div>
       <footer class="footer">
