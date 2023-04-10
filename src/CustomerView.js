71c3c21e7676b71649df57e783a2e58bece87739
Amine Sakrout <sakroutamine@gmail.com>
2023-04-10 16:21:19 -0500

    Fixed server connect

--- src/CustomerView.js
+++ src/CustomerView.js
@@ -1,5 +1,4 @@
-import { useState, useEffect } from 'react';
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import './App.css'; // Tell webpack that App.js uses these styles
@@ -12,44 +11,49 @@ import './App.css'; // Tell webpack that App.js uses these styles
 export default function CustomerView(props) {
   // const [countNum, changeCount] = useState(0);
   const [isLoaded, setIsLoaded] = useState(false);
-  //const [baseItems, setItems] = useState([]);
-  const baseItems = [{item: 'Cheese Pizza', id: 1},
-                    {item:'1 Topping Pizza', id: 2},
-                    {item:'2-4 Topping Pizza', id: 3},
-                    {item:'Drink', id: 4}];
+  const [baseItems, setItems] = useState([]);
+  // const baseItems = [{item_display_name: 'Cheese Pizza', itemtype_id: 1},
+  //                   {item_display_name:'1 Topping Pizza', itemtype_id: 2},
+  //                   {item_display_name:'2-4 Topping Pizza', itemtype_id: 3},
+  //                   {item_display_name:'D', itemtype_id: 4}];
 
 
-  // from https://legacy.reactjs.org/docs/faq-ajax.html
-  // useEffect(() => {
-  //   fetch("http://127.0.0.1:5000/baseItems")
-  //     .then((res) => res.json())
-  //     .then((res) => {
-  //       setIsLoaded(true);
-  //       setItems(res);
-  //     },
-  //       (error) => {
-  //         setIsLoaded(true);
-  //         console.log("error:", error)
-  //         alert(error);
-  //       })
-  // }, [])
+  //from https://legacy.reactjs.org/docs/faq-ajax.html
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/baseItems")
+      .then((res) => res.json())
+      .then((res) => {
+        setIsLoaded(true);
+        setItems(res);
+      },
+        (error) => {
+          setIsLoaded(true);
+          console.log("error:", error)
+          alert(error);
+        })
+  }, [])
 
 
-  const listItems = baseItems.map(baseItems => 
-  <div class="card__container">
-    <div class = "card">
-      <button class="cardContent" key={baseItems.id} onClick={() => props.onFormSwitch("toppings_tabs")}> 
-        {baseItems.item}
-      </button>
-    </div>
-  </div>);
+
 
   return (
     //Parent Element
     <div>
       <div class="background">
         <h2>Choose your Main</h2>
-        {listItems}
+        {isLoaded ? 
+          baseItems.map(item => 
+            <div class="card__container">
+              <div class = "card">
+                <button class="cardContent" key={item.itemtype_id} onClick={() => props.onFormSwitch("toppings_tabs")}> 
+                  {item.item_display_name}
+                </button>
+              </div>
+            </div>)
+            :
+            (<h1>
+              Loading...
+            </h1>)}
       </div>
       <footer class="footer">
         <p>Copyright &copy; 2023 Yezen Hijazin</p>
