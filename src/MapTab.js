913c5af4c213269159ac67835b80ac411c4b5394
Yezen Hijazin <yth77@tamu.edu>
2023-04-27 11:34:24 -0500

    added various files and pictures

---
+++ src/MapTab.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MapContainer from './mapview';
+
+
+export default function MapTab(){
+    return(
+        <div>
+            {MapContainer}
+        </div>
+    );
+}
