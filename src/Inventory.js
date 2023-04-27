ad429eff7c25477bce459ae1d0f7425bce126fda
Isaiah Munoz <isaiahmunoz2002@tamu.edu>
2023-04-26 23:35:02 -0500

    Inventory editable table

--- src/Inventory.js
+++ src/Inventory.js
@@ -3,36 +3,54 @@ import * as React from 'react';
 import { TextField, Button, Typography, Paper, Table, TableBody, TableCell, TableHead, TableRow, TableContainer, Grid } from "@material-ui/core";
 import './ManagerView.css'; // Tell webpack that App.js uses these styles
 
-// Will later connect to back end to have the inventory items in the table
-function createMenuData(menuItem, menuPrice, id, isModifier) {
-    return { menuItem, menuPrice, id, isModifier};
-  }
-  //INPUTS Test for Menu Table
-  const menuRows = [
-    createMenuData('1 Topping Pizza', 6.25, 'OT',false),
-    createMenuData('Cheese Pizza', 6.25, 'CZ', false),
-    createMenuData('Cauliflower Crust', 2.25, 'CC', true),
-    createMenuData('Spinach', 0.0, 'SP', true)
-    
-  ];
 
-// Stock items in the table
-function createStockData(stockId, stockName, quantity, minAmount, units) {
-    return { stockId, stockName, quantity, minAmount, units };
-  }
-  //INPUTS Test for Menu Table
-  const stockRows = [
-    createStockData('WA', 'Water', 15, 0, "lbs"),
-    createStockData('MB', 'Meatball', 4, 0, "lbs"),
-    createStockData('SP', "Spinach", 10, 0, "lbs")
-  ];  
+
 
 function Inventory(props) {
+  /// useState for Stock 
+  const [stockRowIndex, stockSetRowIndex] = useState(-1);
+  const [stockColumnIndex, stockSetColumnIndex] = useState(-1);
+  const [stockRows, stockSetRows] = useState([
+    { stockItem: "Water", quantity: 15, minAmount: 0, units: 'lbs', id: 'WA' },
+    { stockItem: "Meatball", quantity: 4, minAmount: 1,  units: 'lbs', id: 'MB' },
+    { stockItem: "Spinach", quantity: 10, minAmount: 0,  units: 'lbs', id: 'SP' },
+  ]);
+  // Text Field Change for Stock Data
+  const stockTextFieldChange = (rowInd, colName, value) => {
+    stockRows[rowInd][colName] = value;
+  };
+
+  // Sets Back in case of Error Stock Data
+  const stockHandleExit = () => {
+    stockSetRowIndex(-1);
+    stockSetColumnIndex(-1);
+  }
+
+  /// useState for Menu Data
+  const [menuRowIndex, menuSetRowIndex] = useState(-1);
+  const [menuColumnIndex, menuSetColumnIndex] = useState(-1);
+  const [menuRows, menuSetRows] = useState([
+      { menuItem: "1 Topping Pizza", menuPrice: 6.25, isModifier: false, id: 'OT' },
+      { menuItem: "Cheese Pizza", menuPrice: 6.25, isModifier: false, id: 'CZ' },
+      { menuItem: "Cauliflower Crust", menuPrice: 2.25, isModifier: true, id: 'CC' },
+      { menuItem: "Spinach", menuPrice: 0.0, isModifier: true, id: 'SP' },
+  
+  ]);
+  // Text Field Change for Menu Data
+  const handleTextFieldChange = (rowInd, colName, value) => {
+    menuRows[rowInd][colName] = value;
+  };
+
+  // Sets Back in case of Error Menu Data
+  const handleExit = () => {
+    menuSetRowIndex(-1);
+    menuSetColumnIndex(-1);
+  }
+
+
 
   // not working exit button
   const exit = <div class = "exit"><button onClick={() => props.onFormSwitch("manager_view")} type="submit" class="exit_text">Exit</button></div>
- 
-
 
   return (
     //&nbsp; is a character entity that denotes a non-breaking or fixed space
@@ -40,9 +58,141 @@ function Inventory(props) {
     {exit}
     <Typography variant="h2" align='center' style={{marginBottom:"3%", color:"#8458B3"}}>Inventory</Typography>
     
-    <Grid container rowSpacing={1} columnSpacing={{ xs: 1}}>
+    <Grid container columnSpacing={{ xs: 1}}>
+
     
-    {/*Menu Items*/}
+    {/*Menu Table*/}
+    <Grid item xs={6}>
+    <TableContainer component={Paper} style={{margin: '10px', width:700}}>
+    <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead style={{backgroundColor:"#8458B3", fontFamily: 'monospace'}}>
+          <TableRow>
+            <TableCell>Menu Item</TableCell>
+            <TableCell sx={{width: 100}}>Price&nbsp;($)</TableCell>
+            <TableCell sx={{width: 100}}>isModifier</TableCell>
+            <TableCell sx={{width: 100}}>ID</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {menuRows.map((row, index) => (
+              <TableRow key={row.menuItem}>
+                <TableCell onClick={() => { menuSetRowIndex(index); menuSetColumnIndex(0); }}>
+                  {row.menuItem}
+                </TableCell>
+                <TableCell onClick={() => { menuSetRowIndex(index); menuSetColumnIndex(0); }}>
+                  {
+                    menuRowIndex === index && menuColumnIndex === 0 ?
+                      <TextField
+                        placeholder={row.menuPrice}
+                        defaultValue={menuRows[index]["menuPrice"]}
+                        onChange={(event) => handleTextFieldChange(index, "menuPrice", event.target.value)}
+                        onKeyPress={(e) => {
+                          if (e.key === "Enter") {
+                            handleExit();
+                          }
+                        }}
+                      /> : row.menuPrice
+                  }
+                </TableCell>
+                <TableCell>
+                  {String(row.isModifier)}
+                </TableCell>
+                <TableCell>
+                  {row.id}
+                </TableCell>
+
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </Grid>
+
+    {/*Stock Table */}
+    <Grid item xs={6}>
+    <TableContainer component={Paper} style={{margin: '10px', width:700}}>
+    <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead style={{backgroundColor:"#8458B3", fontFamily: 'monospace'}}>
+          <TableRow>
+            <TableCell>Stock Item</TableCell>
+            <TableCell sx={{width: 100}}>Quantity</TableCell>
+            <TableCell sx={{width: 100}}>Min Amount</TableCell>
+            <TableCell sx={{width: 100}}>Units</TableCell>
+            <TableCell sx={{width: 100}}>ID</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {stockRows.map((row, index) => (
+              <TableRow key={row.stockItem}>
+                <TableCell>
+                  {row.stockItem}
+                </TableCell>
+                <TableCell onClick={() => { stockSetRowIndex(index); stockSetColumnIndex(0); }}>
+                  {
+                    stockRowIndex === index && stockColumnIndex === 0 ?
+                      <TextField
+                        placeholder={row.menuItem}
+                        defaultValue={stockRows[index]["quantity"]}
+                        onChange={(event) => handleTextFieldChange(index, "quantity", event.target.value)}
+                        onKeyPress={(e) => {
+                          if ((e.key === "Enter") ) {
+                            stockHandleExit();
+                          }
+                        }}
+                      /> : row.quantity
+                  }
+                </TableCell>
+                <TableCell onClick={() => { stockSetRowIndex(index); stockSetColumnIndex(0); }}>
+                  {
+                    stockRowIndex === index && stockColumnIndex === 0 ?
+                      <TextField
+                        placeholder={row.menuItem}
+                        defaultValue={stockRows[index]["minAmount"]}
+                        onChange={(event) => handleTextFieldChange(index, "minAmount", event.target.value)}
+                        onKeyPress={(e) => {
+                          if ((e.key === "Enter") ) {
+                            stockHandleExit();
+                          }
+                        }}
+                      /> : row.minAmount
+                  }
+                </TableCell>
+                <TableCell onClick={() => { stockSetRowIndex(index); stockSetColumnIndex(0); }}>
+                  {
+                    stockRowIndex === index && stockColumnIndex === 0 ?
+                      <TextField
+                        placeholder={row.menuItem}
+                        defaultValue={stockRows[index]["units"]}
+                        onChange={(event) => handleTextFieldChange(index, "units", event.target.value)}
+                        onKeyPress={(e) => {
+                          if ((e.key === "Enter") ) {
+                            stockHandleExit();
+                          }
+                        }}
+                      /> : row.units
+                  }
+                </TableCell>
+                <TableCell>
+                  {row.id}
+                </TableCell>
+
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </Grid>
+
+    </Grid>
+    </body>
+  );
+}
+export default Inventory;
+
+/**
+ *     <Grid container rowSpacing={1} columnSpacing={{ xs: 1}}>
+    
+
     <Grid item xs={6}>
     <TableContainer component={Paper} style={{margin: '10px', width:700}}>
     <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
@@ -73,7 +223,7 @@ function Inventory(props) {
     </TableContainer>
     </Grid>
 
-    {/*Stock Table*/}
+
     <Grid item xs={6}>
     <TableContainer component={Paper} style={{margin: '10px', width:700}}>
     <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
@@ -108,12 +258,7 @@ function Inventory(props) {
 
 
     </Grid>
-    </body>
-  );
-}
-export default Inventory;
-
-
+ */
 
 
   
