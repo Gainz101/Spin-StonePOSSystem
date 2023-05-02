47c846c3dfc2b58ee8537db2f7f3bd42d0e033d6
Joel Herzog <jherzog@tamu.edu>
2023-05-02 02:59:21 +0000

    Remove build folders

--- backend2/build/ItemType.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ItemType {
-}
-exports.default = ItemType;
-//# sourceMappingURL=ItemType.js.map
--- backend2/build/db/ItemType.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dbGetItemTypes = void 0;
-async function dbGetItemTypes(dbConn) {
-    return (await dbConn.sqlQuery(`SELECT * FROM itemtypes;`));
-}
-exports.dbGetItemTypes = dbGetItemTypes;
-//# sourceMappingURL=ItemType.js.map
