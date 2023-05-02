a6fe1ecc1404caf798c2ea2cb9b9efccd6df9dbb
grubsteak <11185290+grubsteak@users.noreply.github.com>
2023-05-02 16:37:08 -0500

    Update checkout view

--- src/CheckOut.js
+++ src/CheckOut.js
@@ -3,42 +3,83 @@ import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import './App.css'; // Tell webpack that App.js uses these styles
+import { BACKEND_IP } from './BackendConnection';
 
-/**
- * 
- * @param {*} props 
- * @returns Creates a checkout cart menu for our customer side
- */
-export default function CheckOut(props){
-    return(
-    <div class="background">
-        <div class="payBackground">
-            <div class ="itemBoxContainer">
-                <div class ="itemBox">
-                    <div class="contentName">
-                        <strong>2-4 Topping Pizza</strong>
-                        <div class ="contentPrice">$2.00</div>
-                    </div>
-                    <div class ="itemBoxContentEditRemove">
-                        <a onClick={() => props.onFormSwitch("customer_view")}>Edit </a><a>Remove</a>
-                        <div class ="itemBoxContentQTY"><strong>Qty 1</strong></div>
+export default function CheckOut({
+    currentOrder,
+    setCurrentOrder,
+    onFormSwitch,
+    setNewOrder,
+    setItemType,
+}) {
+    return (
+        <div class="background">
+            <div class="payBackground">
+                <div class="itemBoxContainer">
+                    {
+                        currentOrder.items.map(({ item_display_name, item_price, modifiers, item_id }) => {
+                            return <div class="itemBox">
+                                <div class="contentName">
+                                    <strong>{item_display_name}</strong>
+                                    <div class="contentPrice">${item_price}</div>
+                                </div>
+                                <div class="itemBoxContentEditRemove">
+                                    {/* <a onClick={() => props.onFormSwitch("customer_view")}>Edit </a> */}
+                                    <a onClick={()=>{
+                                        fetch(`${BACKEND_IP}/order/removeItem?order_id=${currentOrder.order_id}&item_id=${item_id}`)
+                                        .then((res) => res.json())
+                                        .then((responseJSON) => {
+                                          // Refresh the component with the new order items by setting the order state
+                                          setCurrentOrder(responseJSON)
+                                        })
+                                    }}>Remove</a>
+                                    
+                                    {/* Don't show quantities */}
+                                    {/* <div class="itemBoxContentQTY"><strong>Qty 1</strong></div> */}
+                                </div>
+                            </div>
+                        })
+                    }
+                </div>
+                <div class="subtotalContainer">
+                    <div class="subtotalBox">
+                        <div class="contentName">
+                            <strong>Subtotal</strong>
+                            <div class="contentPrice">${currentOrder.subtotal}</div>
+                        </div>
+                        <div class="contentName">
+                            <strong>Taxes</strong>
+                            <div class="contentPrice">${currentOrder.taxes}</div>
+                        </div>
+                        <div class="contentName">
+                            <strong>Total</strong>
+                            <div class="contentPrice">${currentOrder.total}</div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="subtotalContainer">
-                <div class="subtotalBox">
-                    <div class="contentName">
-                        <strong>Subtotal</strong>
-                        <div class ="contentPrice">$2.00</div>
-                    </div>
+                <div class="finish_order">
+                    <button class="finish_orderText" onClick={() => {
+                        /* Todo: actually complete order */
+                        setNewOrder().then(()=>{
+                            onFormSwitch("topping_tabs")    
+                        })
+                    }}>
+                        Pay
+                    </button>
                 </div>
-            </div>
-            <div class="finish_order">
-                <button class="finish_orderText" onClick={() => props.onFormSwitch("customer_view")}>
-                    Pay
-                </button>
+
+                
+                <div class="finish_order"> {/*Todo: change CSS class? */}
+                    <button class="finish_orderText" onClick={() => {
+                        /* Todo: actually complete order */
+                        setItemType(null)
+                        onFormSwitch("topping_tabs")
+                    }}>
+                        {/* Rename this? */}
+                        Order more
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
     );
 }
