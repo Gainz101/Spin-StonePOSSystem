310b672e3c5978327775017bf1074d9b575b6646
Yezen Hijazin <yth77@tamu.edu>
2023-05-02 17:28:55 -0500

    accesibility

--- src/Login.js
+++ src/Login.js
@@ -25,7 +25,7 @@ import oauth from './oauth';
 e.g.
 
 */
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0"></meta>
 
 /**
  * @returns urlParams
