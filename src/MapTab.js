c14fd8fdf3a544a134ab3d92fbabe90eeb098932
noah-hahn1212 <noah_hahn1212@tamu.edu>
2023-05-02 20:23:19 -0500

    documentation for maptabs

--- src/MapTab.js
+++ src/MapTab.js
@@ -1,6 +1,10 @@
 import React from "react";
 import MapContainer from './mapview';
 
+/**
+ * 
+ * @returns returns a page that has the entire map container for the tabs on customer view
+ */
 export default function MapTab(){
     //Fills page with map function that calls map
     return(
