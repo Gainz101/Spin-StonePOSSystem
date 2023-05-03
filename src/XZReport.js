9d833cc3fc9ebba46ba0b710672f7161d2abc42a
grubsteak <11185290+grubsteak@users.noreply.github.com>
2023-05-02 20:44:46 -0500

    implement xz report

--- src/XZReport.js
+++ src/XZReport.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, TextField, Typography, Grid, Paper} from '@material-ui/core';
 import './ManagerView.css';
 
@@ -10,11 +10,42 @@ import './ManagerView.css';
  * @returns Returns the XZ Report Table 
  */
 const XZReport = (props) => {
+
+  const [Zstate, setZstate] = useState(null);
+  const [Xstate, setXstate] = useState(null);
+
+
+  function getRecentZ(){
+    return new Promise((resolve,fail)=>fetch(`http://zeta.ddns.net/showRecentZ`).then((res) => res.json()).then((recentZ) => {
+      resolve(recentZ.map((zItem)=>{
+        const {itemtype_id, item_display_name, quantity_sold, amount_sold_dollars} = zItem;
+
+    
+
+        return [<p>
+          <span></span>
+        <b>{item_display_name}</b><br/>
+        <b>&nbsp;&nbsp;&nbsp;Quantity Sold: {quantity_sold}</b><br/>
+        <b>&nbsp;&nbsp;&nbsp;Amount Sold: ${amount_sold_dollars.toFixed(2)}</b>
+        
+        </p>]
+      }).flat())
+    }, fail))
+  }
+
+  useEffect(() => {
+  
+  }, [])
+
   const OnCurrentXReportButtonPressed = () => {
+    getRecentZ().then((recentZ)=>setXstate(recentZ))
     // handle current X report button press
   };
 
   const OnOKButtonPressed = () => {
+    getRecentZ().then((recentZ)=>setXstate(recentZ)).then(()=>{
+      fetch(`http://zeta.ddns.net/updateLastZ`).then((res)=>res.json(), alert)
+    })
     // handle OK button press
   };
 
@@ -36,7 +67,7 @@ const XZReport = (props) => {
       
         <Typography variant="h2" align='justify'>X Report</Typography>
         <Paper elevation={1} style={{width: 400, height: 400, overflow: "auto", marginTop:"1%" }} >
-          <p class = "xzPaper"> X - Report </p>
+          {Xstate}
         </Paper>
         <button class =  "yellowbtn" onClick={OnCurrentXReportButtonPressed} style={{marginTop: "3%"}}>Current X Report</button>
       
@@ -45,7 +76,7 @@ const XZReport = (props) => {
       <Grid item xs={6}>
       <Typography variant="h2" align='justify'>Z Report</Typography>
         <Paper elevation={1} style={{width: 400, height: 400, overflow: "auto", marginTop:"1%"}} >
-          <p class = "xzPaper"> Z - Report </p>
+          {Zstate}
         </Paper>
         <button class =  "yellowbtn" onClick={OnCurrentZReportButtonPressed} style={{marginTop: "3%"}}>Current Z Report</button>
         <TextField
